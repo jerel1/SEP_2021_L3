4ddd68fa9725f8589907638483470e36ad121773
jerel1 <203665f@mymail.nyp.edu.sg>
2021-11-30 08:57:52 +0800

    sdl 4(1)

--- views/customer/profile.php
+++ views/customer/profile.php
@@ -4,6 +4,16 @@
 <body>
     <section>
     <!-- Replace the following h1 with code snippet 4-10 -->
-    <h1>To be implemented later</h1>
+    <h1>Profile page</h1>
+    <?php 
+        $user = $_SESSION["loggedInUser"];
+        $customer = getCustomerRecord($user["id"]);
+    ?>
+    Name: <?=$user["name"];?><br>
+    Email: <?=$user["email"];?><br>
+    Mobile Number: <?=$customer["mobileNumber"];?><br>
+    Address: <?=$customer["address"];?><br>
+    Reward Points: <?=$customer["reward_points"];?><br>
+    <a href="updateProfile.php"><button>Update Profile</button></a>
     </section>
 </body>
--- views/logout.php
+++ views/logout.php
@@ -2,7 +2,12 @@
     session_start();
 
     // code snippet 4-8
-    
+    require_once "../controllers/ordersController.php";
+
+    $user = $_SESSION["loggedInUser"];
+    if($user["role"]=="customer") {
+    removeOutstandingOrders();
+}
     session_destroy();
     header("Location: ../index.php");
 ?>
