947dad1e677131292a6ba9090533a4dcf505cbdd
ENZIISBOH <enziboh@gmail.com>
2021-12-14 09:39:37 +0800

    Update header.php

--- views/header.php
+++ views/header.php
@@ -10,7 +10,7 @@
     <header>
         <nav>
             <ul>
-                <li><a href="/delivery_jerel">Home</a></li>
+                <li><a href="/delivery">Home</a></li>
                 <?php
                     session_start();
                     if(isset($_SESSION['loggedInUser'])) {
@@ -20,25 +20,25 @@
                         $role = $user["role"];
                         switch($role) {
                             case "admin":
-                                echo "<li><a href='/delivery_jerel/views/admin/all_orders.php'>See all orders</a></li>";
-                                echo "<li><a href='/delivery_jerel/views/admin/rewards.php'>Rewards</a></li>";
+                                echo "<li><a href='/delivery/views/admin/all_orders.php'>See all orders</a></li>";
+                                echo "<li><a href='/delivery/views/admin/rewards.php'>Rewards</a></li>";
                                 break;
                             case "customer":
-                                echo "<li><a href='/delivery_jerel/views/customer/profile.php'>Profile</a></li>";
-                                echo "<li><a href='/delivery_jerel/views/customer/cart.php'>Cart</a></li>";
-                                echo "<li><a href='/delivery_jerel/views/customer/redeem_rewards.php'>Redeem Rewards</a></li>";
-                                echo "<li><a href='/delivery_jerel/views/customer/past_orders.php'>See past orders</a></li>";
+                                echo "<li><a href='/delivery/views/customer/profile.php'>Profile</a></li>";
+                                echo "<li><a href='/delivery/views/customer/cart.php'>Cart</a></li>";
+                                echo "<li><a href='/delivery/views/customer/redeem_rewards.php'>Redeem Rewards</a></li>";
+                                echo "<li><a href='/delivery/views/customer/past_orders.php'>See past orders</a></li>";
                                 break;
                             case "rider":
-                                echo "<li><a href='/delivery_jerel/views/rider/profile.php'>Profile</a></li>";
-                                echo "<li><a href='/delivery_jerel/views/rider/available_jobs.php'>View Available Jobs</a></li>";
-                                echo "<li><a href='/delivery_jerel/views/rider/completed_jobs.php'>View Completed Jobs</a></li>";
+                                echo "<li><a href='/delivery/views/rider/profile.php'>Profile</a></li>";
+                                echo "<li><a href='/delivery/views/rider/available_jobs.php'>View Available Jobs</a></li>";
+                                echo "<li><a href='/delivery/views/rider/completed_jobs.php'>View Completed Jobs</a></li>";
                                 break;
                         }
-                        echo "<li><a href='/delivery_jerel/views/logout.php'>Logout</a></li>";
+                        echo "<li><a href='/delivery/views/logout.php'>Logout</a></li>";
                     } else {
-                        echo "<li><a href='/delivery_jerel/views/login.php'>Login</a></li>";
-                        echo "<li><a href='/delivery_jerel/views/register.php'>Register</a></li>";
+                        echo "<li><a href='/delivery/views/login.php'>Login</a></li>";
+                        echo "<li><a href='/delivery/views/register.php'>Register</a></li>";
                     }
                 ?>
             </ul>
