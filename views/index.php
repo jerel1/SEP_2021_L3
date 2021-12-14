81d40ccc2399050d930ee428cdb50c160b7b3998
Ethan <94153419+Ethannnnnnnnnnnnnnnn@users.noreply.github.com>
2021-12-14 09:42:02 +0800

    Update index.php

--- views/index.php
+++ views/index.php
@@ -1,7 +1,6 @@
 <?php
-    require_once "./header.php";
+    require_once "./views/header.php";
 
-    // code snippet 1-3
     if(isset($_SESSION["loggedInUser"])) {
         $user = $_SESSION["loggedInUser"];
         $role = $user["role"];
@@ -9,36 +8,15 @@
             header("Location: ./views/admin/index.php");
         } else if($role=="merchant") {
             header("Location: ./views/merchant/index.php");
-        }  else if($role=="rider") {
+        } else if($role=="rider") {
             header("Location: ./views/rider/index.php");
         }
     }
 ?>
 <body>
     <!-- code snippet 4-13 -->
-    <section class="searchSection">
-    <form method="post">
-        <input type="text" name="search" id="search" size="40" placeholder="Search restaurants">
-        <input type="submit" value="Search">
-    </form>
-</section>
     <section class="restaurants">
     <!-- Replace the following h1 with code snippet 4-1 -->
-    <?php
-    foreach($restaurants as $restaurant) {
-?>
-        <article class="restaurant">
-            <img class="restaurant_img" src="<?='./views/uploaded_images/'.$restaurant['image']?>" alt="<?=$restaurant['image']?>">
-            <aside class="restaurantDetails">
-                <h3><a href="./views/customer/viewRestaurant.php?id=<?=$restaurant["id"]?>"><?=$restaurant['name']?></a></h3>
-                <i><?=$restaurant['address']?></i><br><br>
-                Opened from <?=date("h:i A",strtotime($restaurant["open_hours"]))?> to <?=date("h:i A",strtotime($restaurant["close_hours"]))?><br><br>
-                Type of cuisine: <strong><?=$restaurant['cuisine']?></strong><br>
-                Website: <a href="<?=$restaurant['website']?>" target="_blank"><?=$restaurant['website']?></a><br>
-            </aside>
-        </article>
-<?php         
-    }
-?>
+    <h1>To be implemented later</h1>
     </section>
 </body>
