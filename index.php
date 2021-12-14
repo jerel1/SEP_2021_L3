70131d10d76cf1f433f187a29627d9f5ec300a94
Tharsh2003 <tharshinimohanadass@gmail.com>
2021-12-14 18:36:44 +0800

    removed section pertaining to restraurant for now as it causes error

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         }
     }
 ?>
+
 <body>
     <!-- code snippet 4-13 -->
     <section class="searchSection">
@@ -21,24 +22,8 @@
         <input type="text" name="search" id="search" size="40" placeholder="Search restaurants">
         <input type="submit" value="Search">
     </form>
-    </section>
-    <section class="restaurants">
-    <!-- Replace the following h1 with code snippet 4-1 -->
-    <?php
-    foreach($restaurants as $restaurant) {
-    ?>
-            <article class="restaurant">
-                <img class="restaurant_img" src="<?='./views/uploaded_images/'.$restaurant['image']?>" alt="<?=$restaurant['image']?>">
-                <aside class="restaurantDetails">
-                    <h3><a href="./views/customer/viewRestaurant.php?id=<?=$restaurant["id"]?>"><?=$restaurant['name']?></a></h3>
-                    <i><?=$restaurant['address']?></i><br><br>
-                    Opened from <?=date("h:i A",strtotime($restaurant["open_hours"]))?> to <?=date("h:i A",strtotime($restaurant["close_hours"]))?><br><br>
-                    Type of cuisine: <strong><?=$restaurant['cuisine']?></strong><br>
-                    Website: <a href="<?=$restaurant['website']?>" target="_blank"><?=$restaurant['website']?></a><br>
-                </aside>
-            </article>
-    <?php         
-        }
-    ?>
-    </section>
+
+
+
 </body>
+
