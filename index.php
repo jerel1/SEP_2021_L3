ac3bc334596e9975dfa2ed2b2e60d59be9e26cd6
Xanders <202720G@mymail.nyp.edu.sg>
2021-12-14 08:54:31 +0800

    Revert "Update index.php"
    This reverts commit 2b7b9aa6972e6dea575c5e1c2c91da401f9e8c5b.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    require_once "./views/header.php";
-    require_once "./controllers/restaurantController.php";
+    require_once "../header.php";
+    require_once "../../controllers/restaurantController.php";
 ?>
 <body>
     <section>
