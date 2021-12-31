ab823ab6e4c57900f6ca975ade18eb829457f248
Tharsh2003 <tharshinimohanadass@gmail.com>
2021-12-31 08:59:31 +0800

    index page
    Index.php

--- views/register.php
+++ views/register.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="SEP_2021_L3/views/css/Register.css">
+    <link rel="stylesheet" href="css/Register.css">
 
     <?php
     require_once "./header.php";
