58cb1d3eb31354d936a68f4a672d67caa1b76543
Tharsh2003 <tharshinimohanadass@gmail.com>
2022-01-11 08:59:57 +0800

    Create SuccessRegister.php
    After registration customer should see a  message

---
+++ views/SuccessRegister.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/delivery/views/css/Register.css">
+    
+
+    <?php
+    require_once "./header.php";
+    ?>
+</head>
+
+
+
+<body>
+
+   <div id="Success">
+       <p>Registration <br> Successful!</p>
+   </div>
+   <div id="Homepage">
+       <a href="index.php"><p>Go to homepage</p></a>
+   </div>
+    
+    <div class="fixed">
+        <footer id="socialmedia">
+            <a href="https://twitter.com/?lang=en"><img src="uploaded_images/twitter.png" alt="Twitter" width="80px" height="80px"></a>
+            <a href="https://www.facebook.com/fareastfloracomsg"><img src="uploaded_images/Facebook.png" alt="Facebook" width="80px" height="80px"></a>
+            <a href="https://www.instagram.com/fareastfloracomsg/"><img src="uploaded_images/Instagram.png" alt="Instagram" width="95px" height="95px"></a>
+        </footer>
+    </div>
+</body>
+
+</html>
