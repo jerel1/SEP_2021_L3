85a8f01290ca6ec34222e6b8897b961c7aa66e71
Tharsh2003 <tharshinimohanadass@gmail.com>
2022-01-13 20:58:25 +0800

    Past Orders and Redeem Rewards
    PastOrders and RedeemRewards page

--- views/customer/past_orders.php
+++ views/customer/past_orders.php
@@ -1,8 +1,119 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/SEP_2021_L3/views/css/PastOrders.css">
+    <?php
     require_once "../header.php";
-?>
+    ?>
+</head>
 <body>
+
+    <h1>Your Order History</h1>
     <section>
-    <h1>To be implemented by students</h1>
+    <div class="box">
+
+    <div id="flex">
+        <div>
+        <div class="imagebox">
+            Image of order
+        </div>
+        <br>
+        <p class="orderInfo">Order Info</p>
+        </div>
+
+        <div>
+            <p>Delivery Date</p>
+        </div>
+
+        <div>
+            <p>Delivery Time</p>
+        </div>
+
+        <div>
+            <p>Cost</p>
+        </div>
+
+        <div>
+            <p>Total Amount Spent</p>
+        </div>
+
+        <div>
+            <p>Quantity</p>
+        </div>
+    </div>
+    </div>
+
+    <div class="box">
+
+    <div id="flex">
+        <div>
+        <div class="imagebox">
+            Image of order
+        </div>
+        <br>
+        <p class="orderInfo">Order Info</p>
+        </div>
+
+        <div>
+            <p>Delivery Date</p>
+        </div>
+
+        <div>
+            <p>Delivery Time</p>
+        </div>
+
+        <div>
+            <p>Cost</p>
+        </div>
+
+        <div>
+            <p>Total Amount Spent</p>
+        </div>
+
+        <div>
+            <p>Quantity</p>
+        </div>
+    </div>
+    </div>
+
+    <div class="box">
+
+    <div id="flex">
+        <div>
+        <div class="imagebox">
+            Image of order
+        </div>
+        <br>
+        <p class="orderInfo">Order Info</p>
+        </div>
+
+        <div>
+            <p>Delivery Date</p>
+        </div>
+
+        <div>
+            <p>Delivery Time</p>
+        </div>
+
+        <div>
+            <p>Cost</p>
+        </div>
+
+        <div>
+            <p>Total Amount Spent</p>
+        </div>
+
+        <div>
+            <p>Quantity</p>
+        </div>
+    </div>
+    </div>
+
     </section>
 </body>
+</html>
--- views/customer/redeem_rewards.php
+++ views/customer/redeem_rewards.php
@@ -1,6 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/SEP_2021_L3/views/css/Rewards.css">
+    <?php
     require_once "../header.php";
-?>
+    ?>
+
+</head>
 
 <body>
     <section>
@@ -15,14 +25,21 @@
         $rewardsNotRedeemed = getUnredeemedRewards($user["id"]);
         $customer = getCustomerRecord($user["id"]);
     ?>
-    <h1>Rewards Page</h1>
+    <h1>My Rewards</h1>
     <p>
         You have <?=$customer["reward_points"]?> reward points now.
     </p>
+    
+
+   
+   <div class="box">
     <?php
         if(count($rewardsRedeemed)==0) {
             echo "<h3>You have not redeemed any rewards.</h3>";
-        } else {
+        }
+        
+
+        else {
             echo "<h3>You have redeemed the following rewards:</h3>";
             echo "<ul>";
             foreach ($rewardsRedeemed as $reward) {
@@ -34,20 +51,20 @@
         if(count($rewardsNotRedeemed)==0) {
             echo "<h3>There are no available awards to be redeemed.</h3>";
         } else {
-            echo "<h3>These are the available rewards which you can redeem:</h3>";
+            echo "<br><h3>You may proceed to redeem the rewards that are available</h3>";
     ?>
-            <table border="1">
-                <tr>
-                <th>Description</th>
-                <th>Cash Value</th>
-                <th>Redeem Points</th>
-            </tr>
+
+     </div>       
+     <div class="box2">
+                
+            
             <?php
                 foreach ($rewardsNotRedeemed as $reward) {
                     echo "<tr>";
-                    echo "<td>".$reward["description"]."</td>";
-                    echo "<td>$".$reward["cash_value"]."</td>";
-                    echo "<td>".$reward["redeem_points"]."</td>";
+                    
+                    echo "<td>".$reward["description"]."</td><br><br>";
+                    echo "Cash Value :$".$reward["cash_value"]."<br><br>";
+                    echo "Redeem Points :".$reward["redeem_points"]."<br><br>";
                     echo "<td>
                         <form method='post' action='../../controllers/rewardRedemptionController.php?function=add'>
                             <input type='hidden' name='reward_id' value=".$reward["id"].">
@@ -57,13 +74,25 @@
                         </td>";
                     echo "</tr>";
                 }
-            echo "</table>"; 
+          
         }   //end else
         if(isset($_SESSION["redeem_message"])) {
             echo $_SESSION["redeem_message"];
             unset($_SESSION["redeem_message"]);
         }
-        ?>
+    ?>
+    </div>
+
+    <div class="box2">
+       <p>Rewards Details</p>
+    </div>
+
+    <div class="box2">
+       <p>Rewards</p>
+    </div>
+  
 </section>
-    </section>
+</section>
+
 </body>
+</html>
