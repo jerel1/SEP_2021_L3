654fe79dcca6e839d813d0e0cb0b6a32b690e985
Tharsh2003 <tharshinimohanadass@gmail.com>
2022-01-16 12:50:50 +0800

    Index.php can now display uploaded images

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
     require_once "./views/header.php";
+    require_once "controllers/restaurantController.php";
+    require_once "controllers/itemController.php";
+    
 
     // code snippet 1-3
     if(isset($_SESSION["loggedInUser"])) {
@@ -22,6 +25,27 @@
         <input type="text" name="search" id="search" size="40" placeholder="Search restaurants">
         <input type="submit" value="Search">
     </form>
+</section>
+<?php
+
+    $restaurants=getAllRestaurants();
+    foreach($restaurants as $restaurant) {
+
+?>
+        <article class="restaurant">
+            <img class="restaurant_img" src="<?='./views/uploaded_images/'.$restaurant['image']?>" alt="<?=$restaurant['image']?>">
+            <aside class="restaurantDetails">
+                <h3><a href="./views/customer/viewRestaurant.php?id=<?=$restaurant["id"]?>"><?=$restaurant['name']?></a></h3>
+                <i><?=$restaurant['address']?></i><br><br>
+                Opened from <?=date("h:i A",strtotime($restaurant["open_hours"]))?> to <?=date("h:i A",strtotime($restaurant["close_hours"]))?><br><br>
+                Type of cuisine: <strong><?=$restaurant['cuisine']?></strong><br>
+                Website: <a href="<?=$restaurant['website']?>" target="_blank"><?=$restaurant['website']?></a><br>
+            </aside>
+        </article>
+<?php         
+    }
+?>
+
 
 
 
