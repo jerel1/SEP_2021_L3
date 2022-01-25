144dc348f7e1f765905ed662bf285c03bebe9e22
jerel1 <203665f@mymail.nyp.edu.sg>
2022-01-25 08:48:08 +0800

    add your home pages after login in logincontroller

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -7,13 +7,28 @@
     if(!empty($_POST)) {
         $email = $_POST["email"];
         $password = $_POST["password"];
+        $role = $_SESSION["role"];
         $query = mysqli_query($conn,"SELECT * from users where email='$email' and password=MD5('$password')");
         $result = mysqli_fetch_assoc($query);
         //check if result contains something.
         if($result) {
             $_SESSION["loggedInUser"] = $result;
-            header("Location: /delivery");
-        } else {
+            if($role ="merchant"){
+                header("Location:../views/merchant/index.php");
+            }
+            else if($role="admin"){
+            
+            }
+            else if($role = "customer"){
+
+            }
+            else if($role = "rider")
+        
+        }
+        else if($role = "admin"){
+
+        }      
+        else {
             $_SESSION["loginErrorMessage"] = "You have entered the wrong login credentials. Please try again.";
             header("Location: ../views/login.php");
         }
