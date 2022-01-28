83f092ec99578fbe091c596bb773cfd3059eae33
Tharsh2003 <tharshinimohanadass@gmail.com>
2022-01-28 08:12:15 +0800

    index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
     require_once "./views/header.php";
-    require_once "controllers/restaurantController.php";
+    require_once "./controllers/restaurantController.php";
     require_once "controllers/itemController.php";
-    
 
     // code snippet 1-3
     if(isset($_SESSION["loggedInUser"])) {
@@ -12,42 +11,45 @@
             header("Location: ./views/admin/index.php");
         } else if($role=="merchant") {
             header("Location: ./views/merchant/index.php");
-        } else if($role=="rider") {
+        }  else if($role=="rider") {
             header("Location: ./views/rider/index.php");
         }
     }
-?>
-
-<body>
-    <!-- code snippet 4-13 -->
-    <section class="searchSection">
-    <form method="post">
-        <input type="text" name="search" id="search" size="40" placeholder="Search restaurants">
-        <input type="submit" value="Search">
-    </form>
-</section>
-<?php
-
-    $restaurants=getAllRestaurants();
-    foreach($restaurants as $restaurant) {
-
-?>
-        <article class="restaurant">
-            <img class="restaurant_img" src="<?='./views/uploaded_images/'.$restaurant['image']?>" alt="<?=$restaurant['image']?>">
-            <aside class="restaurantDetails">
-                <h3><a href="./views/customer/viewRestaurant.php?id=<?=$restaurant["id"]?>"><?=$restaurant['name']?></a></h3>
-                <i><?=$restaurant['address']?></i><br><br>
-                Opened from <?=date("h:i A",strtotime($restaurant["open_hours"]))?> to <?=date("h:i A",strtotime($restaurant["close_hours"]))?><br><br>
-                Type of cuisine: <strong><?=$restaurant['cuisine']?></strong><br>
-                Website: <a href="<?=$restaurant['website']?>" target="_blank"><?=$restaurant['website']?></a><br>
-            </aside>
-        </article>
-<?php         
+    
+    if(isset($_POST['search']))
+    {
+        $restaurants = searchRestaurants($_POST['search']);
+    } else {
+        $restaurants = getAllRestaurants();
     }
 ?>
+<body>
+<?php
+        if(isset($_POST['search']) && $_POST['search']!="") {
+            echo "Showing search results for \"". $_POST['search'] . "\"<br><br>";
+        }
+    ?>
+    
+ 
+    <section class="restaurants">
 
-
-
+    <?php
+        foreach($restaurants as $restaurant) {
+    ?>
+            <article class="restaurant">
+                <img class="restaurant_img" src="<?='./views/uploaded_images/'.$restaurant['image']?>" alt="<?=$restaurant['image']?>">
+                <aside class="restaurantDetails">
+                    <h3><a class="restaurantName" href="./views/customer/viewRestaurant.php?id=<?=$restaurant["id"]?>"><?=$restaurant['name']?></a></h3>
+                    <i><?=$restaurant['address']?></i><br><br>
+                    Opened from <?=date("h:i A",strtotime($restaurant["open_hours"]))?> to <?=date("h:i A",strtotime($restaurant["close_hours"]))?><br><br>
+                    Type of cuisine: <strong><?=$restaurant['cuisine']?></strong><br>
+                    Website: <a href="<?=$restaurant['website']?>" target="_blank"><?=$restaurant['website']?></a><br>       
+                    Average Rating: <?=$restaurant['Rating']?>
+                </aside>
+            </article>
+    <?php         
+        }
+    ?>
+    </section>
 
 </body>
-
